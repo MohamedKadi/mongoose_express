0ec9b7f2308ed736f50f7037c279df9c5b21258a
MohamedKadi <gamesmohamed14@gmail.com>
2024-09-16 16:59:18 +0100

    set up

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
+    console.log('db is connected');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    res.send('hi how r u');
+})
+
+app.listen(3000, ()=>{
+    console.log("APP IS LISTENING ON PORT 3000");
+})
